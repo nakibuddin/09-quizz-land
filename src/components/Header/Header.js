af4bf0ddcdd733604d99d968cbd9c12d9f942b24
nakibuddin <nakibaust137@gmail.com>
2022-10-12 15:07:57 +0600

    Header component added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,10 +3,9 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className='header'>
-            {/* <br /> <br /> <br /> <br /> <br /> <br /> <br />
+        <div className='header'>            
             <h1>Never stop learning,</h1>
-            <h2>This website will help you to enlarge your knowledge.</h2> */}
+            <h2>This website will help you to enlarge your knowledge.</h2>
 
         </div>
     );
